2db525c84695364b8c37206d0b66229a11cff89d
andersonsouza90 <andersonsilva.souza90@gmail.com>
2021-06-30 23:19:42 -0300

    first commit

---
+++ src/test/java/com/apirest/SpringbootApplicationTests.java
@@ -0,0 +1,12 @@
+package com.apirest;
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@org.junit.Test
+	void contextLoads() {
+	}
+
+}
